1290439af3776ef0079145b2bb915930a11a0bdf
MATEO MARQUEZ PATI�O <mateomarquez315389@correo.itm.edu.co>
2023-02-19 10:53:17 -0500

    Creating new concret class called SalaryEmployee

---
+++ MyFirstCodeOPP/MyFirstCodeOPP/SalaryEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOPP
+{
+    public class SalaryEmployee : Employee
+    {
+        #region Properties
+        public decimal Salary { get; set; }
+
+        #endregion
+
+        #region Method
+        public override decimal GetValueToPay()
+        {
+            return Salary;  
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} \n\t" +
+                $" Value To Pay:{GetValueToPay()}";
+        }
+        #endregion
+
+
+    }
+}
